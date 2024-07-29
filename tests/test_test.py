76dcf4cbb1b80e23d7218ce245ca81c47b7ade8d
Abid <abdur.abid@madetech.com>
2024-07-29 16:55:49 +0100

    test length of properties

--- tests/test_test.py
+++ tests/test_test.py
@@ -1,2 +1,14 @@
+import json
+from src.utils import get_properties_from_os
+
+
 def test_pytest_works():
     assert True == True
+
+
+def test_length_of_properties():
+    properties = None
+    with open('properties.json') as json_properties:
+        data = json.load(json_properties)
+        properties = get_properties_from_os(data)
+    assert len(properties) == 4
