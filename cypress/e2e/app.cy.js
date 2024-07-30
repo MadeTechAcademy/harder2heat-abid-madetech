fb6d78f4db61b8e8b05c866399e682fb31afabec
Abid <abdur.abid@madetech.com>
2024-07-30 12:19:01 +0100

    move properties count to p tag in html body

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,6 +1,6 @@
 describe('My Flask App', () => {
   it('Should render the index page', () => {
     cy.visit('http://127.0.0.1:5000/')
-    cy.contains('Properties : 4');
+    cy.get('body p').contains('Properties : 4');
   })
 })
