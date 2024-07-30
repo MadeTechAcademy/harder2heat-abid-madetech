8774a6f1646559e2c42e3937963dc4a2223e9db9
Abid <abdur.abid@madetech.com>
2024-07-30 12:04:05 +0100

    refactor

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,5 +1,5 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('My Flask App', () => {
+  it('Should render the index page', () => {
     cy.visit('http://127.0.0.1:5000/')
     cy.contains('Properties : 4');
   })
