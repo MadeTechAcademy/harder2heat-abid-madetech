995b89957a649a62499df841dd605bc82ccc159c
Abid <abdur.abid@madetech.com>
2024-08-06 12:19:34 +0100

    add table to home page with the correct headers

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,6 +1,20 @@
 describe('My Flask App', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5000/');
+  });
+
   it('Should render the index page', () => {
-    cy.visit('http://127.0.0.1:5000/')
     cy.get('body p').contains('Properties : 4');
   })
+
+  it('should have a table with the correct headers', () => {
+    cy.get('table').should('exist');
+
+    const headers = ['OSID', 'Building Coordinates', 'Building Size', 'Building Age Updated Date', 'Connectivity'];
+    headers.forEach((header, index) => {
+      cy.get('table thead tr th')
+        .eq(index)
+        .should('contain.text', header);
+    });
+  });
 })
