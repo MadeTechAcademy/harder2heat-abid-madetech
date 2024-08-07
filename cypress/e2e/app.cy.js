fc446cefcaed12938b5dd6dcc9eb72f6d8a60b8d
Abid <abdur.abid@madetech.com>
2024-08-07 12:54:48 +0100

    update column name

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,6 +1,6 @@
 describe('My Flask App', () => {
   beforeEach(() => {
-    cy.visit('http://127.0.0.1:5000/');
+    cy.visit('http://127.0.0.1:3000/');
   });
 
   it('Should render the index page', () => {
@@ -11,7 +11,7 @@ describe('My Flask App', () => {
   it('should have a table with the correct headers', () => {
     cy.get('table').should('exist');
 
-    const headers = ['OSID', 'Building Coordinates', 'Building Size', 'Building Age Updated Date', 'Connectivity'];
+    const headers = ['OSID', 'Building Coordinates', 'Size', 'Building Age Updated Date', 'Connectivity'];
     headers.forEach((header, index) => {
       cy.get('table thead tr th')
         .eq(index)
