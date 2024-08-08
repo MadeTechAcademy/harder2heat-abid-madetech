5586cba50dbaee44240579e5e50a4c6b29f751bd
Abid <abdur.abid@madetech.com>
2024-08-08 11:59:59 +0100

    refactor property init method

--- src/property.py
+++ src/property.py
@@ -5,13 +5,13 @@
 
 
 class Property:
-    def __init__(self, uprn):
+    def __init__(self, uprn, connectivity=None, coordinates=None, osid=None, size=None, age_updated_date=None):
         self.uprn = uprn
-        self.connectivity = None
-        self.coordinates = None
-        self.osid = None
-        self.size = None
-        self.age_updated_date = None
+        self.connectivity = self.modify_connectivity_description(connectivity)
+        self.coordinates = coordinates
+        self.osid = osid
+        self.size = size
+        self.age_updated_date = age_updated_date
 
     @staticmethod
     def modify_connectivity_description(connectivity_description):
